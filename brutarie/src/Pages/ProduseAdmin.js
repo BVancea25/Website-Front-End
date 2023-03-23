bcd0469b8acdd76721efc5bcec8fff67ee5ea4ca
BVancea25 <b_vancea@yahoo.com>
2023-03-23 21:43:59 +0200

    added unstyled form to ProduseAdmin component

--- brutarie/src/Pages/ProduseAdmin.js
+++ brutarie/src/Pages/ProduseAdmin.js
@@ -3,14 +3,25 @@ import ProdusAdmin from "./ProdusAdmin";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import useAuth from "../hooks/useAuth";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const ProduseAdmin = () => {
   const [Setproduse, setData] = useState();
   const { auth } = useAuth();
+  const [msg, setMsg] = useState();
+  const [formData, setFormData] = useState({
+    nume: "",
+    gramaj: "",
+    pret: "",
+    descriere: "",
+  });
+
+  const [imageData, setImage] = useState("");
 
   useEffect(() => {
     axios
-      .get("http://localhost:3500/produse", {
+      .get("/produseAdmin", {
         headers: { Authorization: "Bearer " + auth.accessToken },
       })
       .then((res) => {
@@ -19,11 +30,50 @@ const ProduseAdmin = () => {
       .catch((error) => {
         console.error(error);
       });
-  }, [auth.accessToken, Setproduse]);
+  }, [Setproduse, auth.accessToken]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    console.log(name + " " + value);
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleImageChange = (event) => {
+    setImage(event.target.files[0]);
+  };
 
   if (Setproduse === undefined) {
     return <div>Loading...</div>;
   }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData();
+
+    const { nume, gramaj, pret, descriere } = formData;
+
+    data.append("nume", nume);
+    data.append("gramaj", gramaj);
+    data.append("pret", pret);
+    data.append("descriere", descriere);
+    data.append("image", imageData);
+
+    await axios
+      .post("/produse", data, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then(setMsg("Produsul a fost adaugat !"))
+      .catch((error) => {
+        console.log(error);
+        setMsg("Eroare !!!");
+      });
+  };
+
   const produse = Setproduse.map((produs) => {
     return (
       <ProdusAdmin
@@ -38,7 +88,72 @@ const ProduseAdmin = () => {
     );
   });
 
-  return <div>{produse}</div>;
+  return (
+    <div>
+      {produse}
+
+      <div>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formNume">
+            <Form.Label>Nume produs:</Form.Label>
+            <Form.Control
+              name="nume"
+              value={formData.nume}
+              onChange={handleChange}
+              type="text"
+              placeholder="nume"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formGramaj">
+            <Form.Label>Gramaj:</Form.Label>
+            <Form.Control
+              name="gramaj"
+              value={formData.gramaj}
+              onChange={handleChange}
+              type="text"
+              placeholder="gramaj"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formPret">
+            <Form.Label>Pret:</Form.Label>
+            <Form.Control
+              name="pret"
+              value={formData.pret}
+              onChange={handleChange}
+              type="text"
+              placeholder="pret"
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formDescriere">
+            <Form.Label>Descriere:</Form.Label>
+            <Form.Control
+              name="descriere"
+              value={formData.descriere}
+              as="textarea"
+              onChange={handleChange}
+              rows={5}
+              placeholder="Introduceti o descriere a produsului ..."
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formImage">
+            <Form.Label>Imagine</Form.Label>
+            <Form.Control
+              value={formData.image}
+              onChange={handleImageChange}
+              type="file"
+              label="Alege o imagine jpg"
+              accept="image/jpeg"
+            />
+          </Form.Group>
+          <Button type="submit">Submit</Button>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default ProduseAdmin;
