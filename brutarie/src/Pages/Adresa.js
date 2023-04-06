9bc4a821584b060653b7e7c4de9f2cb37f1fa1e1
BVancea25 <b_vancea@yahoo.com>
2023-04-06 10:23:05 +0300

    implemented post for locations on admin side

--- brutarie/src/Pages/Adresa.js
+++ brutarie/src/Pages/Adresa.js
@@ -1,26 +1,17 @@
 import React from "react";
 import Button from "react-bootstrap/esm/Button";
-import useAxiosPrivate from "../hooks/axiosPrivate";
+import "../CSS/Locatie.css";
 
 const Adresa = (props) => {
   const id = props.id;
-  const axiosPrivate = useAxiosPrivate();
 
-  const handleDelete = (id) => {
-    axiosPrivate
-      .delete(`/locatii/${id}`)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  console.log(props);
   return (
-    <div id={id}>
-      <p>{props.adresa}</p>
-      <Button className="badge bg-danger" onClick={() => handleDelete(id)}>
+    <div className="child-adresa-container" id={id}>
+      <p className="child-adresa-text">{props.adresa}</p>
+      <Button
+        className="badge bg-danger"
+        onClick={() => props.handleDelete(id)}
+      >
         Delete
       </Button>
     </div>
--- brutarie/src/Pages/LocatieAdmin.js
+++ brutarie/src/Pages/LocatieAdmin.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
 import useAxiosPrivate from "../hooks/axiosPrivate";
 import Adresa from "./Adresa";
+import "../CSS/Locatie.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const LocatieAdmin = () => {
   const [addresses, setAddress] = useState();
   const axiosPrivate = useAxiosPrivate();
+  const [formData, setForm] = useState({ adresa: "" });
 
   useEffect(() => {
     axiosPrivate
@@ -21,12 +25,93 @@ const LocatieAdmin = () => {
     return <div>Loading...</div>;
   }
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setForm((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const address = new FormData();
+    address.append("address", formData.adresa);
+
+    await axiosPrivate
+      .post("/locatii", address)
+      .then((res) => {
+        console.log(res);
+        axiosPrivate
+          .get("/adrese")
+          .then((res) => {
+            setAddress(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    setForm({ adresa: "" });
+  };
+
+  const handleDelete = async (id) => {
+    await axiosPrivate
+      .delete(`/locatii/${id}`)
+      .then((res) => {
+        console.log(res);
+        axiosPrivate
+          .get("/adrese")
+          .then((res) => {
+            setAddress(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   const adrese = addresses.map((adresa) => {
     console.log(adresa._id);
-    return <Adresa key={adresa._id} id={adresa._id} adresa={adresa.addres} />;
+    return (
+      <Adresa
+        key={adresa._id}
+        id={adresa._id}
+        adresa={adresa.addres}
+        handleDelete={handleDelete}
+      />
+    );
   });
 
-  return <div>{adrese}</div>;
+  return (
+    <div className="admin-container">
+      <div className="adresa-container">{adrese}</div>
+
+      <div className="form">
+        <Form onSubmit={onSubmit}>
+          <Form.Group>
+            <Form.Label>Introduceti o noua adresa:</Form.Label>
+            <Form.Control
+              name="adresa"
+              value={formData.adresa}
+              onChange={handleChange}
+              type="text"
+            ></Form.Control>
+          </Form.Group>
+          <Button className="button" variant="secondary" type="submit">
+            Adauga adresa
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default LocatieAdmin;
